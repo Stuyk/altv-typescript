2d001b63a2284e6be0af1af182a3de11b180b780
BotisDerG <barnekiel@gmail.com>
2022-04-18 13:36:05 +0200

    improved startup.ts

--- src/core/server/startup.ts
+++ src/core/server/startup.ts
@@ -1,12 +1,20 @@
 import * as alt from 'alt-server';
+import './modules/db';
+import { DB } from './modules/db';
+import { WebhookResourceStartedWithDB, WebhookResourceStartedWithDBerr } from './modules/webhook';
 
-alt.log(`alt:V Server - Boilerplate Started`);
 alt.on('playerConnect', handlePlayerConnect);
 
-function handlePlayerConnect(player: alt.Player) {
-    alt.log(`[${player.id}] ${player.name} has connected to the server.`);
+DB.initialize()
+	.then((connection) => {
+		WebhookResourceStartedWithDB();
+	})
+	.catch((err) => WebhookResourceStartedWithDBerr(err));
 
-    player.model = 'mp_m_freemode_01';
-    player.spawn(36.19486618041992, 859.3850708007812, 197.71343994140625, 0);
-    alt.emitClient(player, 'log:Console', 'alt:V Server - Boilerplate Started');
+function handlePlayerConnect(player: alt.Player) {
+	alt.log(`[${player.id}] ${player.name} has connected to the server.`);
+
+	player.model = 'mp_m_freemode_01';
+	player.spawn(36.19486618041992, 859.3850708007812, 197.71343994140625, 0);
+	alt.emitClient(player, 'log:Console', 'alt:V Server - Boilerplate Started');
 }
